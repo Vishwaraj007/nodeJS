31bedce847000b16460663227ed5ce4b3737a082
Vishwaraj007 <vishwaraj2375@gmail.com>
2026-06-15 15:51:09 +0530

    Grade system added

--- projects/1. Student Grade Calculator/main.js
+++ projects/1. Student Grade Calculator/main.js
@@ -79,5 +79,32 @@ cal.addEventListener("click", () => {
         let percentage = (markSum/(70*nom))*100;
         document.getElementById("percentage").innerHTML = ""+percentage.toFixed(2)+"%.";
 
+        let grade = "";
+        if(percentage <=100 && percentage>=90)
+        {
+            grade += "A+";
+        }
+        else if(percentage < 90 && percentage >= 80)
+        {
+            grade += "A";
+        }
+        else if(percentage < 80 && percentage >= 70)
+        {
+            grade += "B";
+        }
+        else if(percentage < 70 && percentage >= 60)
+        {
+            grade += "C";
+        }
+        else if(percentage < 60 && percentage >= 50)
+        {
+            grade += "D";
+        }
+        else if(percentage < 50)
+        {
+            grade += "F";
+        }
+
+        document.getElementById("grade").innerHTML = "GRADE : "+grade;
     }
 });
