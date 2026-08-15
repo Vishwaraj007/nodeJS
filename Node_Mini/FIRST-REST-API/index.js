8709c838b0f93c75c860c274baaf2a1ce72a835c
Vishwaraj007 <vishwaraj2375@gmail.com>
2026-08-16 02:24:38 +0530

    FIRST REST API COMPLETED.

--- Node_Mini/FIRST-REST-API/index.js
+++ Node_Mini/FIRST-REST-API/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const users = require("./MOCK_DATA.json");  //importing the users...
+const fs = require("fs");
 const app = express();
+
+//middlewere for the form data.....it converts the form data into unstructured(key : value) format
+app.use(express.urlencoded({extended : false}));
 
 //1) GET /users -----> display all the user(for browser : responce in html(ssr))
 app.get("/users",(req,res) => {
@@ -59,6 +63,65 @@ app.get("/users/:id",(req,res) => {
     `;
     res.send(html);
 
+});
+
+
+// 4) POST /api/users -----> add new user.
+app.post("/api/users",(req,res) =>{
+    const body = req.body;
+    console.log(body);
+    let current_id = users.length+1;
+    let user = users.findIndex(user=> user.id === current_id);
+    while(user != -1)
+    {
+        current_id++;
+        user = users.findIndex(user=> user.id === current_id);
+    }
+    users.push({...body,id : current_id});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({status:"completed"});
+    });
+});
+
+// same for the browser
+
+app.post("/users",(req,res)=>{
+    const body = req.body;
+    users.push({...body,id:users.length+1});
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.end("DATA udloaded");
+    })
+});
+
+
+//5) PATCH /api/users/1  --------->   UPDATE the user with id 1
+app.patch("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const body = req.body;
+    let index = users.findIndex(user => user.id === id)
+    users[index] = {...users[index],...body};
+
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.json({status:"success"});
+    });
+});
+
+
+//6) DELETE /api/users/1  -------->  DELETE the user with id 1
+
+app.delete("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const index = users.findIndex(user => user.id === id);
+    if(index == -1){
+        return res.status(404).json({message : "user NOT found"});
+    }
+    users.splice(index,1);
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err)=>{
+        if(err){
+            return res.json({message : "error occured"});
+        }
+        return res.json({message : "Record deleted"});
+    });
 });
 
 
